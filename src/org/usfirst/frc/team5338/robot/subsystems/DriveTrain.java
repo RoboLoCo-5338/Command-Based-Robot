a6668c1ad89f86e054134378c06dcda7b62e44e4
Arvind <loudjumpybolt@gmail.com>
2017-01-15 11:18:30 -0500

    Finished Command-Based Robot

--- src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
@@ -18,7 +18,7 @@ public class DriveTrain extends Subsystem {
     private final CANTalon DRIVEL2 = new CANTalon(2);
     private final CANTalon DRIVER1 = new CANTalon(3);
     private final CANTalon DRIVER2 = new CANTalon(4);
-
+    private final double speed = 0.75;
 	private final RobotDrive drive = new RobotDrive(DRIVEL1, DRIVEL2, DRIVER1, DRIVER2);
 
 	public DriveTrain() {
@@ -43,7 +43,7 @@ public class DriveTrain extends Subsystem {
 	 *            Speed in range [-1,1]
 	 */
 	public void drive(double left, double right) {
-		drive.tankDrive(left, right);
+		drive.tankDrive(-speed * left, -speed * right, true);
 	}
 
 	/**
@@ -51,6 +51,6 @@ public class DriveTrain extends Subsystem {
 	 *            The XBOX style joystick to use to drive tank style.
 	 */
 	public void drive(Joystick joy) {
-		drive(joy.getRawAxis(1), joy.getRawAxis(3));
+		drive.tankDrive(-speed * joy.getRawAxis(1), -speed * joy.getRawAxis(3), true);
 	}
 }
