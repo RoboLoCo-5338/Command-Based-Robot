e3c258e45de2e28f25b12ce18be5091d530c1329
Arvind <loudjumpybolt@gmail.com>
2017-01-15 11:49:52 -0500

    Added rumble!

--- src/org/usfirst/frc/team5338/robot/commands/PowerSettingMin.java
+++ src/org/usfirst/frc/team5338/robot/commands/PowerSettingMin.java
@@ -2,15 +2,18 @@ package org.usfirst.frc.team5338.robot.commands;
 
 import org.usfirst.frc.team5338.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
 /**
  *
  */
-public class PowerSettingMin extends Command
+public class PowerSettingMin extends TimedCommand
 {
 
-    public PowerSettingMin() {
+    public PowerSettingMin()
+    {
+    	super(1);
     	requires(Robot.drivetrain);
     }
 
@@ -18,27 +21,13 @@ public class PowerSettingMin extends Command
     protected void initialize()
     {
     	Robot.drivetrain.DRIVE.setMaxOutput(0.5);
+    	Robot.oi.getJoystick().setRumble(RumbleType.kRightRumble, 1);
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() 
-    { 	
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() 
-    {
-        return true;
-    }
 
     // Called once after isFinished returns true
     protected void end()
     {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted()
-    {
+    	Robot.oi.getJoystick().setRumble(RumbleType.kRightRumble, 0);
     }
 }
