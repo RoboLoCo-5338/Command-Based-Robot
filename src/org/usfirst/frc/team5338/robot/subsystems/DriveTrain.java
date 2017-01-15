c7fc51661dd0430682c952e9bc90b9ecb682f0bb
Arvind <loudjumpybolt@gmail.com>
2017-01-15 11:27:48 -0500

    Set additional values.

--- src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
@@ -18,11 +18,13 @@ public class DriveTrain extends Subsystem {
     private final CANTalon DRIVEL2 = new CANTalon(2);
     private final CANTalon DRIVER1 = new CANTalon(3);
     private final CANTalon DRIVER2 = new CANTalon(4);
-    private final double speed = 0.75;
-	private final RobotDrive drive = new RobotDrive(DRIVEL1, DRIVEL2, DRIVER1, DRIVER2);
+	private final RobotDrive DRIVE = new RobotDrive(DRIVEL1, DRIVEL2, DRIVER1, DRIVER2);
 
 	public DriveTrain() {
 		super();
+		//DRIVE.setSensitivity(0.5);
+		DRIVE.setSafetyEnabled(true);
+		DRIVE.setMaxOutput(0.5);
 	}
 
 	/**
@@ -43,7 +45,7 @@ public class DriveTrain extends Subsystem {
 	 *            Speed in range [-1,1]
 	 */
 	public void drive(double left, double right) {
-		drive.tankDrive(-speed * left, -speed * right, true);
+		DRIVE.tankDrive(left, right, true);
 	}
 
 	/**
@@ -51,6 +53,6 @@ public class DriveTrain extends Subsystem {
 	 *            The XBOX style joystick to use to drive tank style.
 	 */
 	public void drive(Joystick joy) {
-		drive.tankDrive(-speed * joy.getRawAxis(1), -speed * joy.getRawAxis(3), true);
+		DRIVE.tankDrive(joy.getRawAxis(1), joy.getRawAxis(3), true);
 	}
 }
