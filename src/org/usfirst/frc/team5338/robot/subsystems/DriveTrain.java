63d5bb34b99e07294316fa8ab55a5aa55c0a2774
RoboLoCo <team5338@gmail.com>
2017-02-04 18:03:45 -0500

    Fixed commit

--- src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
@@ -7,7 +7,6 @@ import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
