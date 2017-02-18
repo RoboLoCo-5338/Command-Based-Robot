0b63799e8f3f9ed00741cd682768687c518d1f12
Arvind <loudjumpybolt@gmail.com>
2017-02-18 17:29:44 -0500

    Moving vision code.

--- src/org/usfirst/frc/team5338/robot/Robot.java
+++ src/org/usfirst/frc/team5338/robot/Robot.java
@@ -68,127 +68,10 @@ public class Robot extends IterativeRobot
 		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
 
+		// Jetson power spark on enable
 		jetsonPower.set(Relay.Value.kOn);
 		Timer.delay(1);
 		jetsonPower.set(Relay.Value.kOff);
-		// Jetson power spark on enable
-//		visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
-//
-//			oldTime = lastObserved.time;
-//			time = System.currentTimeMillis();
-//
-//			ArrayList<Rect> rects = new ArrayList<Rect>();
-//			for (MatOfPoint mop : pipeline.findContoursOutput())
-//					rects.add(Imgproc.boundingRect(mop));
-//
-//			//remove duplicates
-//			Set<Rect> hs = new HashSet<>();
-//			hs.addAll(rects);
-//			rects.clear();
-//			rects.addAll(hs);
-//
-//			//remove rectangles that aren't the right size
-//			for(int i=0;i<rects.size();i++)
-//			{
-//				Rect r = rects.get(i);
-//				if((Math.abs(2.5 - r.height / (float)r.width)>0.5) && (r.y)> 1 )
-//				{
-//					rects.remove(i);
-//					i--;
-//				}
-//			}
-//
-//				// In order for the location algorithms to work, first we need SF2 to
-//				// orient the robot to directly face the reflective tape, or have the camera
-//				// face it
-//
-//				if (!rects.isEmpty()) {
-//
-//					if(rects.size()==2) {
-//						Rect r1 = rects.get(0);
-//						Rect r2 = rects.get(1);
-//						
-//						//observed = new Snapshot(time, (r1.x+r2.x+r1.width+r2.width)/2-IMG_WIDTH/2, (r1.y+r2.y+r1.height+r2.height)/2, Math.abs(r1.x-r2.x));
-//					} else if (time - oldTime < 200) {
-//						//use lastObserved to help determine the new position
-//						//TODO 1 or >3 rectangles
-//						//observed = new Snapshot(0,0,0,0);
-//
-//					} else {
-//						//determine position with rectangle data only
-//						//TODO 1 or >3 rectangles
-//						Rect r1 = rects.get(0);
-//						Rect r2 = rects.get(1);
-//						Rect r3 = rects.get(2);
-//
-//						ArrayList<Rect> rects = new ArrayList<Rect>();
-//
-//						/*
-//						* rect at rects(0) will be the largest rectangle that we see
-//						*/
-//
-//						if(r1.height > r2.height && r1.height > r3.height)
-//						{
-//							rects.add(r1);
-//							rects.add(r2);
-//							rects.add(r3);
-//						}
-//						else if(r2.height > r1.height && r2.height > r3.height)
-//						{
-//							rects.add(r2);
-//							rects.add(r1);
-//							rects.add(r3);
-//						}
-//						else
-//						{
-//							rects.add(r3);
-//							rects.add(r1);
-//							rects.add(r2);
-//						}
-//						//Biggest rectangle is added first
-//
-//						if(rects.get(1).x == rects.get(2).x * 1.02 && rects.get(1).x == rects.get(2).x * 0.98)
-//						{
-//							// dimensions of the rectangle: h: 130.175 w: 50.8 in millimeters, h/w = 2.5625
-//
-//							double HEIGHT_OF_RECTANGLE_IN_PIXELS_WHEN_DOCKED = 300;
-//							// TODO: THIS NEEDS TO BE DETERMINED WITH ACTUAL TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//
-//							double distance = 1 / ((HEIGHT_OF_RECTANGLE_IN_PIXELS_WHEN_DOCKED) / rects.get(0).height);
-//							// calculate distance by doing 1 meter / distance = expected height / height
-//							// need to check if true
-//							double expWidth = rects.get(0).height / 2.5625; // expected width
-//							double angle = asin(rects.get(0).width / expWidth); // find the angle of the robot compared to straight on
-//
-//							int side = 0; // 1 = left of the peg - 2 = right of the peg
-//
-//							if(rects.get(0).x > rects.get(1).x)
-//							{
-//								side = 2;
-//							}
-//							else
-//							{
-//								side = 1;
-//							}
-//						}
-//						//observed = new Snapshot(0,0,0,0);
-//
-//					}
-//				} else {
-//					if (time - oldTime < 500) {
-//						//observed = new Snapshot(lastObserved.time,lastObserved.x,lastObserved.y,lastObserved.width);
-//					} else {
-//						//observed = new Snapshot(0,0,0,0);
-//					}
-//				}
-//
-//			synchronized (imgLock) {
-//				lastObserved = observed;
-//				raw = new ArrayList<Rect>(rects);
-//			}
-//
-//		});
-//		visionThread.start();
 
 		// instantiate the command used for the autonomous period
 		autonomousCommand = new Autonomous();
@@ -210,9 +93,9 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopInit()
 	{
-		jetsonPower.set(Relay.Value.kOn);
-		Timer.delay(1);
-		jetsonPower.set(Relay.Value.kOff);
+//		jetsonPower.set(Relay.Value.kOn);
+//		Timer.delay(1);
+//		jetsonPower.set(Relay.Value.kOff);
 		autonomousCommand.cancel();
 	}
 
