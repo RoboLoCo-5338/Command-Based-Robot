c455c0722a4d21b668e6df434a15b0202e1ecc7a
Arvind <loudjumpybolt@gmail.com>
2017-02-18 19:06:23 -0500

    Fixed main.java

--- jetson/src/main/java/Main.java
+++ jetson/src/main/java/Main.java
@@ -9,8 +9,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
-import org.usfirst.frc.team5338.robot.GripPipeline;
-import org.usfirst.frc.team5338.robot.Snapshot;
+import main.java.GripPipeline;
 
 public class Main
 {
@@ -28,7 +27,7 @@ public class Main
     System.loadLibrary("opencv_java310");
 
     // Connect NetworkTables, and get access to the publishing table
-    NetworkTable.setClientMode();
+    NetworkTable.setServerMode();
     // Set your team number here
     NetworkTable.setTeam(5338);
 
@@ -177,7 +176,6 @@ public class Main
 
     // This creates a CvSource to use. This will take in a Mat image that has had OpenCV operations
     // operations
-    
     CvSource imageSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, IMG_WIDTH, IMG_HEIGHT, 30);
     MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
     cvStream.setSource(imageSource);
@@ -257,43 +255,6 @@ public class Main
       // For now, we are just going to stream the HSV image
       imageSource.putFrame(hsv);
     }
-  }
-
-  private static HttpCamera setHttpCamera(String cameraName, MjpegServer server) {
-    // Start by grabbing the camera from NetworkTables
-    NetworkTable publishingTable = NetworkTable.getTable("CameraPublisher");
-    // Wait for robot to connect. Allow this to be attempted indefinitely
-    while (true) {
-      try {
-        if (publishingTable.getSubTables().size() > 0) {
-          break;
-        }
-        Thread.sleep(500);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-
-    HttpCamera camera = null;
-    if (!publishingTable.containsSubTable(cameraName)) {
-      return null;
-    }
-    ITable cameraTable = publishingTable.getSubTable(cameraName);
-    String[] urls = cameraTable.getStringArray("streams", null);
-    if (urls == null) {
-      return null;
-    }
-    ArrayList<String> fixedUrls = new ArrayList<String>();
-    for (String url : urls) {
-      if (url.startsWith("mjpg")) {
-        fixedUrls.add(url.split(":", 2)[1]);
-      }
-    }
-    camera = new HttpCamera("CoprocessorCamera", fixedUrls.toArray(new String[0]));
-    server.setSource(camera);
-    return camera;
   }
 
   private static UsbCamera setUsbCamera(int cameraId, MjpegServer server) {
